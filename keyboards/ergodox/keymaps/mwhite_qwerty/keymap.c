5c6ff017c838499e9b45a0cff734c28ef6dc26ca
marshallewhite <marshall@marshallwhite.ninja>
2017-01-16 00:37:27 -0500

    Fixes spotlight key

--- keyboards/ergodox/keymaps/mwhite_qwerty/keymap.c
+++ keyboards/ergodox/keymaps/mwhite_qwerty/keymap.c
@@ -37,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   OSM(MOD_HYPR),
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         TG(SYMB),        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_QUOT,        LALT(KC_LSFT),  KC_LEFT,KC_RGHT, KC_LGUI
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   M(1),
+        KC_QUOT,        LALT(KC_LSFT),  KC_LEFT,KC_RGHT, KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_BSPC,KC_DELT,KC_END,
